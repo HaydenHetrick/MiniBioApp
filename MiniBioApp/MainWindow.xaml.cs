f1c63f18924a76dcb7e8fe0e850e273f13f8e582
Hayden Hetrick <hetrickhay@gmail.com>
2024-09-25 12:17:51 -0400

    Add project files.

---
+++ MiniBioApp/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace MiniBioApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MainFrame.Navigate(new Page1()); // Load Page 1 by default
+        }
+
+        private void Button_Click_Page1(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Page1());
+        }
+
+        private void Button_Click_Page2(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Page2());
+        }
+
+        private void Button_Click_Page3(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Page3());
+        }
+    }
+}
---
+++ MiniBioApp/Page3.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MiniBioApp
+{
+    public partial class Page3 : Page
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+
+        private void SubmitButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameTextBox.Text;
+            string email = EmailTextBox.Text;
+            string message = MessageTextBox.Text;
+
+            string data = $"Name: {name}\nEmail: {email}\nMessage: {message}\n\n";
+
+            string filePath = "ContactFormData.txt";
+
+            try
+            {
+                File.AppendAllText(filePath, data);
+                MessageBox.Show("Your message has been submitted successfully!", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                NameTextBox.Clear();
+                EmailTextBox.Clear();
+                MessageTextBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
